658ebb753d0fd29c5859547e19b28c8557784a24
cf2009 <codefuture@hotmail.com>
2009-08-02 15:33:48 +0100

    [Added] New option "Open Playlists" it is now possible to add a playlist to the main/sub menus and have it play or open (for viewing) the playlist...   "Open Playlists" = off (play playlist)   "Open Playlists" = on (open playlist) you will need to set the option (open playlist on/off) before you add the playlist to a menu....

--- extras/favourites.py
+++ extras/favourites.py
@@ -1,55 +1,59 @@
-import xbmc, xbmcgui, os
+import xbmc, xbmcgui, os, sys
 from xbmcgui import Window
 from xml.dom.minidom import parseString
 
 dialog = xbmcgui.DialogProgress()
 
 class Main:
-   # grab the home window
-   WINDOW = Window ( 10004 )
+    # grab the home window
+    WINDOW = Window ( 10004 )
 
-   def __init__( self ):
-      dialog.create("Aeon Mod","Loading and parsing favourites data...")
-      self._clear_properties()
-      self._read_file()
-      self._parse_String()
-      self._fetch_favourites()
-      self.doc.unlink()
-      dialog.close()
+    def __init__( self ):
+        dialog.create("Aeon Mod","Loading and parsing favourites data...")
+        self._clear_properties()
+        self._read_file()
+        self._parse_String()
+        self._fetch_favourites()
+        self.doc.unlink()
+        dialog.close()
 
-   def _clear_properties( self ):
-      for count in range( 20 ):
-         # clear Property
-         self.WINDOW.clearProperty( "favourite.%d.path" % ( count + 1, ) )
-         self.WINDOW.clearProperty( "favourite.%d.name" % ( count + 1, ) )
+    def _clear_properties( self ):
+        for count in range( 20 ):
+            # clear Property
+            self.WINDOW.clearProperty( "favourite.%d.path" % ( count + 1, ) )
+            self.WINDOW.clearProperty( "favourite.%d.name" % ( count + 1, ) )
 
-   def _read_file( self ):
-      # Set path
-      self.fav_dir = 'special://masterprofile//favourites.xml'
-      # Check to see if file exists
-      if (os.path.isfile( self.fav_dir ) == False):
-         self.favourites_xml = '<favourites><favourite name="Can Not Find favourites.xml">-</favourite></favourites>'
-      else:
-         # read file
-         self.fav = open( self.fav_dir , 'r')
-         self.favourites_xml = self.fav.read()
-         self.fav.close()
+    def _read_file( self ):
+        # Set path
+        self.fav_dir = 'special://masterprofile//favourites.xml'
+        # Check to see if file exists
+        if (os.path.isfile( self.fav_dir ) == False):
+            self.favourites_xml = '<favourites><favourite name="Can Not Find favourites.xml">-</favourite></favourites>'
+        else:
+            # read file
+            self.fav = open( self.fav_dir , 'r')
+            self.favourites_xml = self.fav.read()
+            self.fav.close()
 
-   def _parse_String( self ):
-      self.doc = parseString( self.favourites_xml )
-      self.favourites = self.doc.documentElement.getElementsByTagName ( 'favourite' )
+    def _parse_String( self ):
+        self.doc = parseString( self.favourites_xml )
+        self.favourites = self.doc.documentElement.getElementsByTagName ( 'favourite' )
 
-   def _fetch_favourites( self ):
-      # Go through each favourites
-      self.count = 0
-      for self.doc in self.favourites:
-         self.fav_path = self.doc.childNodes [ 0 ].nodeValue
-         # add return 
-         if "10024" not in self.fav_path: self.fav_path = self.fav_path.replace( ')', ',return)' )
-         # set properties
-         self.WINDOW.setProperty( "favourite.%d.path" % ( self.count + 1, ) , self.fav_path )
-         self.WINDOW.setProperty( "favourite.%d.name" % ( self.count + 1, ) , self.doc.attributes [ 'name' ].nodeValue )
-         self.count = self.count+1
+    def _fetch_favourites( self ):
+        # Go through each favourites
+        self.count = 0
+        for self.doc in self.favourites:
+            self.fav_path = self.doc.childNodes [ 0 ].nodeValue
+            # add return 
+            if '10024' not in self.fav_path: self.fav_path = self.fav_path.replace( ')', ',return)' )
+            if (sys.argv[ 1 ] == 'playlists=window'):
+                if 'playlists/music' in self.fav_path: self.fav_path = self.fav_path.replace( 'PlayMedia(', 'ActivateWindow(10502,' )
+                if 'playlists/video' in self.fav_path: self.fav_path = self.fav_path.replace( 'PlayMedia(', 'ActivateWindow(MyVideoLibrary,' )
+            print 'playlists :' + sys.argv[ 1 ]
+            # set properties
+            self.WINDOW.setProperty( "favourite.%d.path" % ( self.count + 1, ) , self.fav_path )
+            self.WINDOW.setProperty( "favourite.%d.name" % ( self.count + 1, ) , self.doc.attributes [ 'name' ].nodeValue )
+            self.count = self.count+1
 
 if ( __name__ == "__main__" ):
     Main()
